1f22fca78f6c495c1a557478c38b75b374ee0639
Danilo <adrian.astudillo@docente.fup.edu.co>
2024-04-04 17:14:04 -0500

    clases java Atleta y Competencia

--- src/Competencia.java
+++ src/Competencia.java
@@ -1,12 +1,17 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+import java.util.ArrayList;
 
-/**
- *
- * @author figur
- */
 public class Competencia {
+    private ArrayList<Atleta> atletas;
+
+    public Competencia() {
+        ArrayList<Atleta> atletas = new ArrayList<Atleta>();
+    }
+    
+    public void nuevoAtleta(){
+        atletas.add(new Atleta("Pepito", "Colombiano",2,4));
+    
+    }
+    
+    
     
 }
