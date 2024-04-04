5605b37018a1d2db70c541ab65514a58730f5d67
Danilo <adrian.astudillo@docente.fup.edu.co>
2024-04-04 17:13:27 -0500

    clases java Atleta y Competencia

--- src/Atleta.java
+++ src/Atleta.java
@@ -1,12 +1,14 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-
-/**
- *
- * @author figur
- */
 public class Atleta {
-    
+    private String nombre;
+    private String nacionalidad;
+    private int hora;
+    private int minutos;
+
+    public Atleta(String nombre, String nacionalidad, int hora, int minutos) {
+        this.nombre = nombre;
+        this.nacionalidad = nacionalidad;
+        this.hora = hora;
+        this.minutos = minutos;
+    }
+        
 }
